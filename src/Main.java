4049892c76f5fe6015401e2fbe5ce8df071aa312
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-02-28 02:55:36 +0530

    Day 1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println(3+8);
+        System.out.println(3+9);
         System.out.println("Hello World");
     }
 }
