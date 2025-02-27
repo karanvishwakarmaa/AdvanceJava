e71d4e204b80faaa050f7e4b7e8b77c746eb3e13
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-02-28 02:39:43 +0530

    Day 1

---
+++ src/BasicJava/Ex01.java
@@ -0,0 +1,17 @@
+package BasicJava;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        float sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        float product = num1 * num2;
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+
+        float difference = num2 - num1;
+        System.out.println("The difference between " + num2 + " and " + num1 + " is: " + difference);
+    }
+}
---
+++ src/BasicJava/literals/Ex03.java
@@ -0,0 +1,50 @@
+package BasicJava.literals;
+
+public class Ex03 {
+
+    public static void main(String[] args) {
+
+
+        /*
+         * Literals in Java
+         *
+         * Java me literals woh fixed values hote hain jo directly code me assign kiye ja sakte hain.
+         * Ye values program execution ke dauraan change nahi hoti hain.
+         */
+
+        // Integer Literals
+        int decimalNumber = 25;  // Decimal (Base 10)
+        int binaryNumber = 0b1101;  // Binary (Base 2)
+        int octalNumber = 031;  // Octal (Base 8)
+        int hexNumber = 0x1A;  // Hexadecimal (Base 16)
+
+        // Floating-Point Literals
+        float pi = 3.14f;  // Float type (suffix 'f' lagana zaroori hai)
+        double price = 99.99;  // Default double type
+        double scientificNotation = 2.5e3;  // 2.5 * 10^3
+
+        // Character Literals
+        char letter = 'A';
+        char digit = '5';
+        char specialChar = '@';
+        char newLine = '\n';  // Escape sequence for new line
+
+        // String Literals
+        String greeting = "Hello, Java!";
+        String numberString = "12345";
+
+        // Boolean Literals
+        boolean isJavaFun = true;
+        boolean isRainy = false;
+
+        // Null Literal
+        String myStr = null;
+
+        /*
+         * Conclusion:
+         * Literals fixed values hote hain jo Java code me directly assign kiye ja sakte hain.
+         * Yeh integer, floating-point, character, string, boolean, aur null type ke ho sakte hain.
+         */
+
+    }
+}
