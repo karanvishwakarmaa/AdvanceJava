b3502e55c58aee2c5b2bf241e162a03be8f3579b
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-02 23:50:55 +0530

    Day 2

--- src/CoreJava/Switch_Statements/Ex18.java
+++ src/CoreJava/Switch_Statements/Ex18.java
@@ -3,12 +3,31 @@ package CoreJava.Switch_Statements;
 public class Ex18 {
     public static void main(String[] args) {
         int n =1;
-        System.out.println("Monday");
-        System.out.println("Tuesday");
-        System.out.println("Wednesday");
-        System.out.println("Thursday");
-        System.out.println("Friday");
-        System.out.println("Saturday");
-        System.out.println("Sunday");
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day number");
+                break;
+        }
     }
 }
