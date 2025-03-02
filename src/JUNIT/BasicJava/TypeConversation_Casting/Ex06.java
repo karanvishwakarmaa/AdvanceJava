a26176bacdf2fc07b7a0d862d23daaa8b038b041
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-02 16:05:55 +0530

    Day 2

---
+++ src/JUNIT/BasicJava/TypeConversation_Casting/Ex06.java
@@ -0,0 +1,9 @@
+package JUNIT.BasicJava.TypeConversation_Casting;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        float num  = 5.6f;
+        int result = (int) num; // casting float to int
+        System.out.println("result: " + result); // output: result: 5
+    }
+}
