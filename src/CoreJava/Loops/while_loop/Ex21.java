1866cb574414d331d33e43cdb5b42cbf847dd7e9
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-04 07:27:07 +0530

    Day 3

---
+++ src/CoreJava/Loops/while_loop/Ex21.java
@@ -0,0 +1,19 @@
+package CoreJava.Loops.while_loop;
+
+public class Ex21 {
+    public static void main(String[] args) {
+        int i =1;
+
+        while (i <= 4) {
+
+            System.out.println("Hii" + i);
+            int j =1;
+            while (j <= 3) {
+                System.out.print("Hello" + j);
+                j++;
+            }
+            i++;
+        }
+        System.out.println("Bye" + i);
+    }
+}
