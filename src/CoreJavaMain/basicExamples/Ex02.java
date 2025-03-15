4b61f0b1ac0090df5793fee8dda0b138803ad374
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-15 23:28:47 +0530

    Day 15 ( Starting All Concepts of Basic JAVA )

---
+++ src/CoreJavaMain/basicExamples/Ex02.java
@@ -0,0 +1,13 @@
+package CoreJavaMain.basicExamples;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
+    public static void main1(String[] args) {
+        System.out.println("Hello!!! 1");
+    }
+    public static void main2(String[] args) {
+        System.out.println("Hello!!!2");
+    }
+}
