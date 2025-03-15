9190320f4e93c97ea4006ae379f92339960a912b
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-15 20:32:57 +0530

    Day 15 ( Starting All Concepts of Basic JAVA )

--- src/CoreJava/CoreJavaMain/basicExamples/Ex01.java
+++ src/CoreJava/CoreJavaMain/basicExamples/Ex01.java
@@ -3,5 +3,6 @@ package CoreJava.CoreJavaMain.basicExamples;
 public class Ex01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hiii!!!!");
     }
 }
